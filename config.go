48034b9663b45c0568ff26da55c461930e9f31e5
MathiasB <git@denbeke.be>
2023-08-20 16:49:21 +0200

    Cleanup config

--- config.go
+++ config.go
@@ -2,36 +2,27 @@ package imap
 
 import (
 	"fmt"
-	"os"
 )
 
 var (
 	defaultIMAPPort    = 143
 	defaultIMAPAddress = fmt.Sprintf(":%d", defaultIMAPPort)
-	defaultDatabaseURL = "sqlite:test.db"
 )
-
-// BuildConfigFromEnv populates a IMAP backend config from env variables
-func BuildConfigFromEnv() *Config {
-	config := &Config{}
-
-	config.IMAPAddress = getEnv("IMAP_ADDRESS", defaultIMAPAddress)
-	config.DatabaseURL = getEnv("DATABASE_URL", defaultDatabaseURL)
-
-	config.TlsCert = getEnv("TLS_CERT", "")
-	config.TlsKey = getEnv("TLS_KEY", "")
-
-	return config
-}
 
 // Config contains all the config for serving the IMAP backend
 type Config struct {
+
+	// IMAPAddress is the address on which the IMAP server will be exposed.
 	IMAPAddress string
-	DatabaseURL string
-	SeedDB      bool
-	TlsCert     string
-	TlsKey      string
-	Debug       bool
+
+	// TlsCert is the public certificate.
+	TlsCert string
+
+	// TlsKey is the certificates private key.
+	TlsKey string
+
+	// Debug flag.
+	Debug bool
 }
 
 // Validate validates whether all config is set and valid
@@ -41,19 +32,5 @@ func (config *Config) Validate() error {
 		return fmt.Errorf("IMAPAddress cannot be empty")
 	}
 
-	if config.DatabaseURL == "" {
-		return fmt.Errorf("DatabaseURL cannot be empty")
-	}
-
 	return nil
-}
-
-// getEnv gets the env variable with the given key if the key exists
-// else it falls back to the fallback value
-func getEnv(key, fallback string) string {
-	value := os.Getenv(key)
-	if len(value) == 0 {
-		return fallback
-	}
-	return value
 }
